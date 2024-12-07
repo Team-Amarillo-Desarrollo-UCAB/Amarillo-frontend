482ab8ef938b8d1c9ba50351dc7521dbbb94c9d9
LeinarKev <kjfernandez.19@est.ucab.edu.ve>
2024-12-06 22:38:41 -0400

    Implementacion de Cupon

---
+++ lib/Cupon/infrastructure/cupon_service_search_by_code.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+import 'package:desarrollo_frontend/Cupon/domain/Cupon.dart';
+import 'package:desarrollo_frontend/Cupon/domain/Cupon_data.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class CuponServiceSearchByCode {
+  final String baseUrl;
+
+  CuponServiceSearchByCode(this.baseUrl);
+
+  Future<Cupon> getCuponByCode(String cuponCode) async {
+    final response =
+        await http.get(Uri.parse('$baseUrl/cupon/one/by/code?code=$cuponCode'));
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = json.decode(response.body);
+
+      final cuponData = CuponData.fromJson(data);
+
+      return Cupon(
+          code: cuponData.code,
+          expirationDate: DateTime.parse(cuponData.expirationDate),
+          amount: double.parse(cuponData.amount));
+    } else {
+      throw Exception('Error al obtener el producto');
+    }
+  }
+}
