57b0c043496b03c0d0076622a2f8efb2afd3da76
rasg55555 <rasalgado.21@est.ucab.edu.ve>
2024-12-06 23:05:19 -0400

    Keep Status in Profile

--- lib/Users/domain/user_profile.dart
+++ lib/Users/domain/user_profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class UserProfile with ChangeNotifier {
   String name;
@@ -19,9 +20,33 @@ class UserProfile with ChangeNotifier {
     );
   }
 
+  /// Actualiza los datos del perfil y guarda en SharedPreferences
   void updateProfile(String newName, String newPhoneNumber) {
     name = newName;
     phoneNumber = newPhoneNumber;
-    notifyListeners(); 
+    notifyListeners();
+    _saveToPreferences();
+  }
+
+  
+  Future<void> _saveToPreferences() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString('user_name', name);
+    prefs.setString('user_phone_number', phoneNumber);
+    prefs.setString('user_email', email);
+  }
+
+  
+  static Future<UserProfile> loadFromPreferences() async {
+    final prefs = await SharedPreferences.getInstance();
+    final name = prefs.getString('user_name') ?? "Carlos Alonzo";
+    final phoneNumber = prefs.getString('user_phone_number') ?? "+58 4261234567";
+    final email = prefs.getString('user_email') ?? "carlos.alonzo@example.com";
+
+    return UserProfile(
+      name: name,
+      email: email,
+      phoneNumber: phoneNumber,
+    );
   }
 }
