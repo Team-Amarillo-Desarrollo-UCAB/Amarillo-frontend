7cb43ee1615b8f7085acf4c486483b7678e48e6b
LeinarKev <kjfernandez.19@est.ucab.edu.ve>
2025-01-17 06:33:12 -0400

    Fix: Imagen 2D #3

--- lib/Producto/presentation/product_individual_view.dart
+++ lib/Producto/presentation/product_individual_view.dart
@@ -99,9 +99,9 @@ class _PerfumeDetailPageState extends State<PerfumeDetailPage> {
                 // Muestra la imagen 2D
                 return Container(
                   padding: const EdgeInsets.all(20),
-                  child: CircleAvatar(
-                    backgroundImage: product.images[0] as ImageProvider<Object>,
-                    radius: 40,
+                  child: Image.network(
+                    product.images[0] as String,
+                    fit: BoxFit.contain,
                   ),
                 );
               }
